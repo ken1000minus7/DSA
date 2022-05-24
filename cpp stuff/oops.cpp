5bc6eb0c53e4ee8e004609d442add09f48fbac89
ken1000minus7 <manjotsinghoberoi.btcse20@pec.edu.in>
2022-05-24 22:16:47 +0530

    Learning const

--- cpp stuff/oops.cpp
+++ cpp stuff/oops.cpp
@@ -12,6 +12,7 @@ class Pokemon{
     string name;
     string type;
     int weight;
+    mutable string lastFunctionCalled;
 
     // constructor - function which constructs an object, called when an object is created
 
@@ -28,8 +29,12 @@ class Pokemon{
 
     // member functions
 
-    void display()
+    // declaring function as const ensures that no data member will be changed inside this function
+    void display() const
     {
+        // however, we can change mutable variables even in a const function
+        lastFunctionCalled = "display";
+        
         cout<<name<<" "<<type<<" "<<weight<<"\n";
     }
 
@@ -43,6 +48,26 @@ int main()
     Pokemon p2("Bulbasaur","Grass",40);
 
     p2.display();
+
+    // const ensure that value of the variable can't be changed
+    const int hi = 10;
+
+    // pointer to a const int, we can't change the value by deferencing, can write int const as well
+    const int * p = &hi;
+    
+    // can change the value like this, but hi variable will still have the same value, very weird behaviour of cpp lol
+    int * pp = (int *) &hi;
+    *pp = 100;
+
+    // can change value of the memory location but the pointer variable a is now const and can't be reassigned
+    int* const a =new int;
+
+    // can't change both
+    const int * const aa = new int;
+
+    // if we declare a const object, we can only use const functions of that object since we can't change any data members (obvio)
+    const Pokemon p3("Charmander","Fire",40);
+    p3.display();
 
     return 0;
 }
