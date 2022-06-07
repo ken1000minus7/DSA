656e04e1812dfc699bef53ca5f6309fddb50d25a
ken1000minus7 <manjotsinghoberoi.btcse20@pec.edu.in>
2022-06-08 02:09:05 +0530

    keep on moving forward

--- kotlin stuff/oops.kt
+++ kotlin stuff/oops.kt
@@ -46,4 +46,52 @@ fun main(){
     println(c.component1()) // return first parameter value, in this case it is name
     // can deconstruct data classes like this, i will store component1, j component2 and so on
     val (i,j) = c
+
+    val a = Thing.Hello()
+    val b = Thing().Bye()
+
+    // Exception is problem or unexpected event that occurs during runtime and can hinder the flow of the program
+    // Unchecked Exception are exceptions that are occur during runtime to problem in our code logic like NullPointerException
+    // They extend from RuntimeException
+    // Checked exceptions are exceptions that are caught during compile time like IOException
+    // They extend from Throwable class
+
+    // Exception handling invloves catching these exceptions when they occur to avoid any hindrance in the flow of the program
+
+    // try block contains the code that can cause an exception
+    try{
+        // throw used to explicitly throw an exception
+        throw Exception("u r nab")
+    }
+    // catch block contains the code that is executed when an exception is caught
+    // can use different catch blocks for different types of exceptions
+    catch(e : ArithmeticException){
+        println("Arithmetic exception")
+    }
+    catch(e : Exception){
+        e.printStackTrace()
+        println("idk")
+    }
+    // finally block is executed even when exception does not occur
+    finally{
+        println("done ig")
+    }
+}
+
+// can only access within the same file
+private class Abc()
+
+// can only access within the same module
+internal class Bcd()
+
+class Thing(){
+    // nested class, class inside another class, can't access members of the upper class, static by default
+    class Hello(){
+
+    }
+
+    // inner calss, class inside class but with inner keyword, can access members of upper class, can't be used in interfaces or nested classes, non static
+    inner class Bye(){
+
+    }
 }
